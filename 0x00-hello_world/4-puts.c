deec9b3d801bc10a6745c0e167f191dc30241039
Nicole2512 <nicolemokaya@gmail.com>
2022-03-10 06:00:55 -0800

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#inlude<stdio.h>
+#include<stdio.h>
 /**
  *  * main - Prints "Programming is like building a multilingual
  *                 puzzle, followed by a new line.
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,5 @@
 #include<stdio.h>
-
+/*
+ * main -  prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ *
+ * REturn
