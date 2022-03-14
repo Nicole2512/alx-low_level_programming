7effbf1c276206887510c3aa424f66a0f86e4a1b
Nicole2512 <nicolemokaya@gmail.com>
2022-03-14 02:51:41 -0700

    fixed bug

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,17 +2,17 @@
 /**
  * main - Prints all base 16 numbers in lowercase
  *
- * Return 0
+ * Return: Always 0
  */
 int main(void)
 {
-	int number ;
+	int number;
 	int letter;
 
-	for (number=0;number<10;number++)
-		putchar((number % 10) +'0');
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
 
-	for(letter='a'; letter <='f'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 
 	putchar('\n');
