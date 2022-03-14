f9f630f8c849dab076783e711bd5b317f57b67b3
Nicole2512 <nicolemokaya@gmail.com>
2022-03-14 02:27:45 -0700

    fixed betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,14 @@
 /**
  * main - Prints al single digit numbers of base 10
  *
- * Return : Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-	int number ;
+	int number;
 
-	for (number = 0; number < 10 ; number++)
-		putchar((number % 10) +'0');
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
 
 	putchar('\n');
 
