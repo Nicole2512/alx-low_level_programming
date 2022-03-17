6dcab30bca0a0663f4355de149d3074d56c77324
Nicole2512 <nicolemokaya@gmail.com>
2022-03-17 03:03:39 -0700

    Task complete

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle using #
+ * @size: size of rectangle
+ */
+void print_triangle(int size)
+{
+	int h, index;
+
+	if (size > 0)
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (index = size - h; index > 0; index--)
+				_putchar(' ');
+
+			for (index = 0; index < h; index++)
+				_putchar('#');
+
+			if (h == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
