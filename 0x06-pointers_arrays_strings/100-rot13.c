4466c53575e1b2e3667cff92a77a6cc8240a6f8d
Nicole2512 <nicolemokaya@gmail.com>
2022-03-24 01:11:17 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - ecodes string using rot13
+ * @s: sring to be encoded
+ *
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotkey[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				s[i] = rotkey[j];
+				break;
+			}
+		}
+
+	}
+	return (s);
+}
